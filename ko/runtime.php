459d8b096b88c1c57a7fa269e30ca161e8aa8b8c
leat3233 <leatitiamboumba2016@gmail.com>
2025-08-27 15:54:30 +0800

    Add files via upload

---
+++ ko/runtime.php
@@ -0,0 +1,11 @@
+<?php
+    if(!defined('CW')){exit('Access Denied');}
+    require "adapter/adapter.php";
+    require "config/global_config.php";
+    require "library/file.php";
+    require "library/functions.php";
+    require "system.php";
+    require "library/dbserver.php";
+    
+    
+?>
